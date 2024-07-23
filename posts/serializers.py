d9cdf0ec841e91d3a7398bf07c16a25562fb52cc
Asya Haroyan <alharoyan@gmail.com>
2024-07-23 09:03:02 +0000

    Add validator

---
+++ posts/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image is lagrer than 2MB!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'The width of the image is larger than 4096px!'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'The height of the image is larger than 4096px!'
+            )
+        return value
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Post
+        fields = [
+            'id', 'owner', 'created_at', 'updated_at', 'title',
+            'content', 'image', 'image_filter'
+        ]
